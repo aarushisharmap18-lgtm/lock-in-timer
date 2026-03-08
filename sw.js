e977a9850318accf51e5cfe69da38d8b2dc29732
aarushisharmap18-lgtm <aarushisharmap18@gmail.com>
2026-03-08 22:00:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", event => {
 self.skipWaiting()
 })
 
-self.addEventListener("fetch", e => {
-e.respondWith(fetch(e.request))
+self.addEventListener("fetch", event => {
+event.respondWith(fetch(event.request))
 })
